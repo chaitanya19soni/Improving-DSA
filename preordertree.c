ff16cba105938d7ca441efe79baaac7384c3a6b8
chaitanya19soni <1839chhaitanya@gmail.com>
2026-03-12 09:48:11 +0530

    tree

--- preordertree.c
+++ preordertree.c
@@ -3,53 +3,54 @@
 
 typedef struct tree {
     char data;
-    struct tree* left ;
-    struct tree*right ;
+    struct tree * left ;
+    struct tree * right ;
 }*Tree;
 
-Tree createNode(char data ){
-    Tree newNode = (Tree)malloc(sizeof(struct tree));
-    newNode -> data =  data;
-    newNode -> left  = NULL;
-    newNode ->right  = NULL;
+Tree createNode(char data){
+    Tree newNode = (Tree)malloc (sizeof(struct tree));
+    newNode ->data= data;
+    newNode ->left =  NULL;
+    newNode -> right =  NULL;
+    return newNode ;
+}
 
-    return newNode;
-}   
-
-void traverse (Tree root ){
-    if (root == NULL ){
-        return;
+void traverse( Tree root ){
+    if (root == NULL)
+    {
+        return ;
     }
-    
-    printf (" %d -> ", root -> data);
-    traverse (root->left);
-    traverse (root->right );
-
+    printf("%d->", root ->data);
+    traverse(root->left );
+    traverse(root->right);
 
 }
 
+
 int main() {
-    Tree root  = createNode('R');
-    Tree nodeA = createNode('A');
-    Tree nodeB = createNode('B');
-    Tree nodeC = createNode('C');
-    Tree nodeD = createNode('D');
-    Tree nodeE = createNode('E');
-    Tree nodeF = createNode('F');
-    Tree nodeG = createNode('G');
+    
+ Tree root =  createNode ('R');
+ Tree nodeA=  createNode ('A');
+ Tree nodeB=  createNode ('B');
+ Tree nodeC=  createNode ('C');
+ Tree nodeD=  createNode ('D');
+ Tree nodeE=  createNode ('E');
+ Tree nodeF=  createNode ('F');
+ Tree nodeG=  createNode ('G');
+ 
+ root ->left = nodeA;
+ root ->right = nodeB;
+ 
+ nodeA->left = nodeC;
+ nodeA->right = nodeD;
 
-    root -> left = nodeA;
-    root -> right  = nodeB;
+ nodeB->left =nodeE;
+ nodeB->right = nodeF;
+ 
+ nodeF->left =  nodeG;
 
-    nodeA -> left = nodeC;
-    nodeA ->right  =nodeD;
 
-    nodeB->left = nodeE;
-    nodeB -> right = nodeF;
-
-    nodeF -> left = nodeG;
-
-    traverse(root);
-
-    return 0;
+ traverse(root);
+ 
+ return 0;
 }
