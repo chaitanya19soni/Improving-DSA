34995df715f3cebb880b46ec462e55c53b867102
chaitanya19soni <1839chhaitanya@gmail.com>
2026-04-30 12:18:41 +0530

    comments

--- practice/practice1.c
+++ practice/practice1.c
@@ -18,7 +18,7 @@ Node createNode (int data ){
 
 void printNode( Node pr ){
     while(pr){
-        printf("%d -> " , pr ->data);
+        printf("%d -> " , pr ->data);   // we use this function to print the Node values to which we assign in the main fucntion 
         pr = pr ->next ;
     }
     printf("NULL\n");
@@ -30,16 +30,16 @@ int main() {
 Node node1 = createNode(56);
 Node node2 = createNode(6);
 Node node3 = createNode(6);
-Node node4 = createNode(523);
+Node node4 = createNode(523);       // creating the node by calling the createNode function and assign the values to the Node
 Node node5 = createNode(578);
  
 node1 ->next = node2 ;
 node2 ->next = node3;
-node3 ->next  = node4;
+node3 ->next  = node4;              // linking the Nodes together to form a final link list 
 node4 -> next =node5;
 node5 ->next =  NULL;
 
-printNode(node1);
+printNode(node1);                       // calling the printNode function to print the values assigned 
 
 
 
